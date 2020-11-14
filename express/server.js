256b0111f8546b8cc7400e9ecdfcfa83cf0ae631
ceceliacreates <ceceliacreates@gmail.com>
2020-11-14 11:06:38 -0500

    comments out cors to break browser access

--- express/server.js
+++ express/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+//const cors = require("cors");
 const ballots = require("./ballots.json");
 
 const app = express();
@@ -41,7 +41,7 @@ router.post("/ballots/:id", function (req, res) {
   }
 });
 
-app.use(cors());
+//app.use(cors());
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router);
 
